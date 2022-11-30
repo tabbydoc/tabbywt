aad047475d3f2b1f2b9e25b962ad88d787842361
savch <61005033+ShyFly46@users.noreply.github.com>
2022-11-30 14:17:15 +0800

    Создание классов MIPS алгоритма

---
+++ src/main/java/com/example/demo/MIPSAlgorithm.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+/* находит критические точки в таблице,
+ * выделяет квадрат с загаловками (headers, CC2)
+ * и с данными (data, CC3)
+ */
+
+public class MIPSAlgorithm {
+
+
+    public TableCoordinates search2(){
+        TableCoordinates cc2 = new TableCoordinates(0,0);
+        return cc2;
+    }
+}
---
+++ src/main/java/com/example/demo/TableCoordinates.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+// если уже есть класс, который описывает координаты ячейки, замените им этот класс
+
+public class TableCoordinates {
+    public int x;
+    public int y;
+
+    public TableCoordinates(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}

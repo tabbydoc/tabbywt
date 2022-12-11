7fe1bddbe929b5f2084232d49041194a9463b223
SergeyStepanovsky <98726536+SergeyStepanovsky@users.noreply.github.com>
2022-12-12 00:03:11 +0800

    Update OneWayCellClissifier.java

--- src/main/java/TableCells/OneWayCellClissifier.java
+++ src/main/java/TableCells/OneWayCellClissifier.java
@@ -14,6 +14,7 @@ public abstract class OneWayCellClissifier {
     public static void classify(Map<Element, TableType> map) {
         Elements RealationalElements = new Elements();
         Elements EntityElements = new Elements();
+
         //Заполняем списки для relational и для entity - это получается списки с таблицами
         for (Map.Entry<Element, TableType> entry : map.entrySet()) {
             if (entry.getValue() == TableType.RELATION) {
@@ -26,25 +27,28 @@ public abstract class OneWayCellClissifier {
         // создаем список для таблиц типа Relational Table
         ArrayList<Table> RelationalTables = new ArrayList<>();
 
+        //заполняем RelationalTables
         for (Element element : RealationalElements) {
             RelationalTables.add(ElementToTable.transfer(element));
         }
 
-        int counter = 0;//счетчик для
-        // отправим некоторые характеристики в ячейки
+        int counter = 0;//счетчик для атрибута
+
+        // отправим характеристики в ячейки
         for (Table table : RelationalTables) {
             List<OneWayCell> cells = (List<OneWayCell>) table.getCells();
             for (OneWayCell oneWayCell : cells) {
                 if (oneWayCell.getRow() == 1) {
-                    oneWayCell.setAttribute(7000 + counter);
+                    oneWayCell.setAttribute(7000 + counter); // 7000 взял из головы, сомневаюсь, насоклько правильно реализовал присвоение атрибутов
                     counter++;
                 }
-                if (oneWayCell.getRow() != 1){
-                    oneWayCell.setRecord(oneWayCell.getRow()); // отправили record
-            }
+                if (oneWayCell.getRow() != 1) {
+                    oneWayCell.setRecord(oneWayCell.getRow()); // отправили record в строки
+                }
             }
         }
-        // создаем список для таблиц типа Entity Table
+
+        // создаем список для таблиц типа Entity Table, далее всё почти также как у RealtionTables
         ArrayList<Table> EntityTables = new ArrayList<>();
 
         for (Element element : EntityElements) {
@@ -58,9 +62,10 @@ public abstract class OneWayCellClissifier {
                     oneWayCell.setAttribute(7000 + counter);
                     counter++;
                 }
-                oneWayCell.setRecord(oneWayCell.getRow()); // отправили record
+                if (oneWayCell.getRow() != 1) {
+                    oneWayCell.setRecord(oneWayCell.getCol()); // отправили record в колнки
+                }
             }
         }
-
     }
 }

697962da461a64e50848c727be373cce05c8e1b1
SergeyStepanovsky <stepanevgen2013@yandex.ru>
2023-01-09 13:11:02 +0800

    Add LocalChecking

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,11 +10,17 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import webreduce.data.TableType;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -94,6 +100,25 @@ public class DemoApplication {
 
     @GetMapping("/LocalCheking")
     public void LocalChek(@RequestParam(value = "url", defaultValue = "") String url) throws Exception {
-        
+readFile();
+    }
+    public static void readFile() throws IOException {
+
+        // read a file
+        Resource resource = new ClassPathResource("HTMLcheck.html");
+
+        // get inputStream object
+        InputStream inputStream = resource.getInputStream();
+
+        // convert inputStream into a byte array
+        byte[] dataAsBytes = FileCopyUtils.copyToByteArray(inputStream);
+
+        // convert the byte array into a String
+        String data = new String(dataAsBytes, StandardCharsets.UTF_8);
+
+        // print the content
+        System.out.println(data);
+
     }
 }
+

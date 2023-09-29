8d12e09dd43df043eb9fb9185126e7d14de89b56
SergeyStepanovsky <98726536+SergeyStepanovsky@users.noreply.github.com>
2023-09-29 11:22:48 +0800

    Update OneWayCellClassifier.java

--- src/main/java/TableCells/OneWayCellClassifier.java
+++ src/main/java/TableCells/OneWayCellClassifier.java
@@ -10,68 +10,60 @@ import static webreduce.data.TableType.ENTITY;
 import static webreduce.data.TableType.RELATION;
 
 public abstract class OneWayCellClassifier {
+    // Флаг, указывающий была ли обработана хотя бы одна таблица типа ENTITY
     public static boolean classify = false;
-    // Классифицируем ячейки Relational таблиц
-    public static void classifyRelationalCells(List<Table> tables) {
-        for (Table table : tables) {
-            if (table.getType() == TableType.RELATION) {
-                List<OneWayCell> cells = (List<OneWayCell>) table.getCells();
-                List<OneWayCell> firstRowCells = new ArrayList<>();
 
-                // Получение ячеек первой строки и установка их как атрибуты.
+    // Метод для классификации ячеек Relational таблиц
+    public static void classifyRelationalCells(List<Table> tables) {
+        // Вызов обобщенного метода classifyCells для классификации ячеек Relational таблиц
+        classifyCells(tables, TableType.RELATION, true);
+    }
+
+    // Метод для классификации ячеек Entity таблиц
+    public static void classifyEntityCells(List<Table> tables) {
+        // Вызов обобщенного метода classifyCells для классификации ячеек Entity таблиц
+        classifyCells(tables, TableType.ENTITY, false);
+    }
+
+    // Обобщенный метод для классификации ячеек таблиц
+    private static void classifyCells(List<Table> tables, TableType type, boolean useRow) {
+        // Проход по всем таблицам из списка
+        for (Table table : tables) {
+            // Если тип таблицы совпадает с заданным типом
+            if (table.getType() == type) {
+                // Получение списка ячеек таблицы
+                List<OneWayCell> cells = (List<OneWayCell>) table.getCells();
+                List<OneWayCell> firstCells = new ArrayList<>();
+
+                // Определение ячеек, которые будут использоваться как атрибуты
                 for (OneWayCell cell : cells) {
-                    if (cell.getRow() == 1) {
+                    // Если мы работаем с Relational таблицами, то используем строки, иначе - колонки
+                    int index = useRow ? cell.getRow() : cell.getCol();
+                    if (index == 1) { // Если это первая строка или колонка, то устанавливаем ячейку как атрибут
                         cell.setAttribute(cell);
-                        firstRowCells.add(cell);
+                        firstCells.add(cell); // Добавляем ячейку в список атрибутов
                     }
                 }
 
-                // Присвоение атрибутов всем ячейкам
-                for (OneWayCell headerCell : firstRowCells) {
-                    for (OneWayCell cell : cells) {
-                        if (cell.getCol() == headerCell.getCol()) {
+                for (OneWayCell cell : cells) {
+                    cell.setRecord(cell.getRow()); // Установка record для каждой ячейки на основе её строки.
+
+                    for (OneWayCell headerCell : firstCells) {
+                        int indexToCompare = useRow ? cell.getCol() : cell.getRow();
+                        // Если индекс совпадает с индексом ячейки-атрибута, то устанавливаем атрибут для ячейки
+                        if (indexToCompare == headerCell.getCol()) {
                             cell.setAttribute(headerCell);
                         }
                     }
                 }
 
-                // Присвоение record всем ячейкам
-                for (OneWayCell cell : cells) {
-                    cell.setRecord(cell.getRow());
+
+                // Если таблица типа ENTITY, то устанавливаем флаг classify в true
+                if (type == TableType.ENTITY) {
+                    classify = true;
                 }
             }
         }
     }
-
-    // Классифицируем ячейки  Entity таблиц
-    public static void classifyEntityCells(List<Table> tables) {
-        for (Table table : tables)
-            if (table.getType() == ENTITY) {
-                 classify = true;
-
-                List<OneWayCell> cells = (List<OneWayCell>) table.getCells();
-                //создадим список ячеек, в которых будут хранится ячейки 1-го столбца
-                List<OneWayCell> firstColCells = new ArrayList<>();
-                //добавим атрибуты в 1-ю строку
-
-                for (int i = 0; i < cells.size(); i++) {
-
-                    if (cells.get(i).col == 1) {
-                        cells.get(i).setAttribute(cells.get(i));
-                        firstColCells.add(cells.get(i)); //1-я строка (заголовки) с атрибутами
-                    }
-                }
-
-                for (int i = 0; i < firstColCells.size(); i++) {
-                    for (int j = 0; j < cells.size() ; j++) {
-                        if (cells.get(j).getRow() == firstColCells.get(i).getRow()){
-                            cells.get(j).setAttribute(firstColCells.get(i));
-                        }
-                    }
-                }
-
-            }
-        else
-            classify = false;
-    }
 }
+
